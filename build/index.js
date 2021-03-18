ffed1e2ba1971e3337f1bb2179edbb9dcc4ab7e8
konojack <konojack@gmail.com>
2021-03-18 21:01:40 +0100

    Create Sort class and implement Bubble Sort algorithm

--- build/index.js
+++ build/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("jdfcss");
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([5, 7, 3, 4, 92]);
+sorter.sort();
