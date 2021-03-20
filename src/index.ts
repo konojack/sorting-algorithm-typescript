d25fc4fea7542b1dd0c8d832fd3cd0d4358b8c83
konojack <konojack@gmail.com>
2021-03-20 20:49:01 +0100

    Adding LinkedList class with methods + move data structures to the directory

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-import { CharactersCollection } from './CharactersCollection';
-import { NumbersCollection } from './NumbersCollection';
+import { LinkedList } from './dataStructures/LinkedList';
+import { CharactersCollection } from './dataStructures/CharactersCollection';
+import { NumbersCollection } from './dataStructures/NumbersCollection';
 import { Sorter } from './Sorter';
 
 const numbersCollection = new NumbersCollection([10, 32, 87, 3, 14]);
 const charactersCollection = new CharactersCollection(
   'jDSKJjdjSjNXNFbfieUWUEIRooROWIWPERdksdklfa'
 );
+
 const sorterNumbers = new Sorter(numbersCollection);
 const sorterChars = new Sorter(charactersCollection);
 sorterNumbers.sort();
 sorterChars.sort();
 console.log(numbersCollection.data);
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(5);
+linkedList.add(6);
+linkedList.add(1);
+linkedList.add(2);
+const sorterList = new Sorter(linkedList);
+sorterList.sort();
+console.log(linkedList.print());
